caabe8348ba0130a80ee9d2373a5ef71cf136b09
wveik <kataev_06@list.ru>
2019-08-22 13:41:48 +0300

    Added Edit View

--- src/grails-app/controllers/ru/yandex/InventoryController.groovy
+++ src/grails-app/controllers/ru/yandex/InventoryController.groovy
@@ -6,6 +6,13 @@ class InventoryController {
         render "Hello world"
     }
 
+    def edit() {
+        def productName = "Breakfast Blend"
+        def sku = "BB01"
+
+        [product : productName, sku : sku]
+    }
+
     def remove() {
         render "Remove"
     }
